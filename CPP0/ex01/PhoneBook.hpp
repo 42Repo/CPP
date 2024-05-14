55c15b0f88ee603d13e4e82eb70cbf6b18bde650
asuc <sucaurel@gmail.com>
2024-05-14 13:25:44 +0200

    Debut ex 01 class contact

--- CPP0/ex01/PhoneBook.hpp
+++ CPP0/ex01/PhoneBook.hpp
@@ -5,8 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: asuc <asuc@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/29 03:20:14 by asuc              #+#    #+#             */
-/*   Updated: 2024/04/29 03:20:14 by asuc             ###   ########.fr       */
+/*   Created: 2024/05/09 22:17:21 by asuc              #+#    #+#             */
+/*   Updated: 2024/05/14 13:15:26 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef __PHONEBOOK_H__
+#define __PHONEBOOK_H__
+
+# include "Contact.hpp"
+
+class PhoneBook : public Contact
+{
+    public:
+        PhoneBook();
+        ~PhoneBook();
+        void add();
+        void search();
+        void print();
+    private:
+        int m_index;
+        Contact m_contacts[8];
+};
+
+
+#endif // __PHONEBOOK_H__
