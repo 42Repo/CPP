7f62bfcdac5e277bd7dd4e460cb159a9ce9eb9b7
asuc <sucaurel@gmail.com>
2024-05-18 17:57:48 +0200

    CPP03/ex02 Fais

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,24 @@
 /*   By: asuc <asuc@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:47:50 by asuc              #+#    #+#             */
-/*   Updated: 2024/05/17 20:47:51 by asuc             ###   ########.fr       */
+/*   Updated: 2024/05/18 17:57:23 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main() {
+    std::string str = "HI THIS IS BRAIN";
+
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+
+    std::cout << "Address of the string: " << &str << std::endl;
+    std::cout << "Address of the string using the pointer: " << stringPTR << std::endl;
+    std::cout << "Address of the string using the reference: " << &stringREF << std::endl;
+
+    std::cout << std::endl << "String: " << str << std::endl;
+    std::cout << "Value of the string using the pointer: " << stringPTR << std::endl;
+    std::cout << "String using the reference: " << stringREF << std::endl;
+    return 0;
+}
