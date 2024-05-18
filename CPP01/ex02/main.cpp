ea0a3fb6ea3d0bff7cdf13a3d1ab61b4293e4440
asuc <sucaurel@gmail.com>
2024-05-18 18:00:55 +0200

    Fix String by ptr

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asuc <asuc@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 20:47:50 by asuc              #+#    #+#             */
-/*   Updated: 2024/05/18 17:57:23 by asuc             ###   ########.fr       */
+/*   Updated: 2024/05/18 18:00:07 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int main() {
     std::cout << "Address of the string using the reference: " << &stringREF << std::endl;
 
     std::cout << std::endl << "String: " << str << std::endl;
-    std::cout << "Value of the string using the pointer: " << stringPTR << std::endl;
+    std::cout << "String using the pointer: " << *stringPTR << std::endl;
     std::cout << "String using the reference: " << stringREF << std::endl;
     return 0;
 }
