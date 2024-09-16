3251e6070c1bd598b85a0fe501868dfdbc1540b2
asuc <sucaurel@gmail.com>
2024-09-16 22:48:03 +0200

    Cleaned up files and enhanced Makefile

--- CPP02/ex00/Fixed.h
+++ CPP02/ex00/Fixed.h
@@ -1,17 +1,5 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Fixed.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asuc <asuc@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/05 22:24:09 by asuc              #+#    #+#             */
-/*   Updated: 2024/07/06 19:07:57 by asuc             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef __Fixed_H__
-#define __Fixed_H__
+#ifndef Fixed_H
+#define Fixed_H
 
 #include <iostream>
 
@@ -29,4 +17,4 @@ class Fixed {
     static const int _fractionalBits = 8;
 };
 
-#endif // __Fixed_H__
+#endif // Fixed_H
