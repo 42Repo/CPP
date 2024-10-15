74a7bdbc0d06b3a0275e9a6ffde8a61538d88bea
asuc <sucaurel@gmail.com>
2024-10-15 16:05:22 +0200

    Refactor Makefile to disable unsafe buffer usage warning & add check to iter

--- CPP07/ex01/iter.h
+++ CPP07/ex01/iter.h
@@ -3,6 +3,8 @@
 #include <iostream>
 
 template <typename T> void iter(T *array, size_t size, void (*f)(T const &)) {
+    if (!array || !f)
+        return;
     for (size_t i = 0; i < size; i++) {
         f(array[i]);
     }
