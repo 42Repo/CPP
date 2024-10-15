89b4e2bb826dd4dd6fc2a180507a7d7a0643dea2
asuc <sucaurel@gmail.com>
2024-10-15 19:02:22 +0200

    Refactor main.cpp to add comprehensive tests for Array class and improve code readability

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,4 +1,7 @@
 #include "Array.h"
+#include <cstdlib>
+
+#define MAX_VAL 750
 
 int main() {
     try {
@@ -33,7 +36,7 @@ int main() {
 
         std::cout << "\nTest de l'accès hors limites (attendu une exception):" << std::endl;
         try {
-            std::cout << arraySized[10] << std::endl; 
+            std::cout << arraySized[10] << std::endl;
         } catch (const std::exception &e) {
             std::cerr << "Exception capturée : " << e.what() << std::endl;
         }
@@ -50,5 +53,42 @@ int main() {
         std::cerr << "Exception capturée dans le main: " << e.what() << std::endl;
     }
 
+    Array<int> numbers(MAX_VAL);
+    int       *mirror = new int[MAX_VAL];
+    std::srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++) {
+        const int value = std::rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++) {
+        if (mirror[i] != numbers[i]) {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try {
+        numbers[-2] = 0;
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+    }
+    try {
+        numbers[MAX_VAL] = 0;
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++) {
+        numbers[i] = std::rand();
+    }
+
+    
+    delete[] mirror;
+    
     return 0;
 }
