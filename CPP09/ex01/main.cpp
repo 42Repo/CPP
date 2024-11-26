3004a8fa33e69f4ecbc4b0865ca80ecef9698eb6
asuc <sucaurel@gmail.com>
2024-11-26 15:35:17 +0100

    Refactor usage of 'std::endl' to '\n'

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -3,15 +3,15 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <expression>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <expression>\n";
         return 1;
     }
 
     try {
         int result = RPN::evaluate(argv[1]);
-        std::cout << result << std::endl;
+        std::cout << result << "\n";
     } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
+        std::cerr << e.what() << "\n";
         return 1;
     }
 
