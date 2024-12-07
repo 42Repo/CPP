4f7ff32a010ce75dadfbb4fb91d3cdffaae207c7
asuc <sucaurel@gmail.com>
2024-12-07 21:33:57 +0100

    Simplify PmergeMe constructors and destructor by removing unnecessary output statements

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -1,17 +1,13 @@
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe() { std::cout << "PmergeMe constructor" << std::endl; }
+PmergeMe::PmergeMe() {}
 
-PmergeMe::PmergeMe(const PmergeMe &other) {
-    std::cout << "PmergeMe copy constructor" << std::endl;
-    *this = other;
-}
+PmergeMe::PmergeMe(const PmergeMe &other) { *this = other; }
 
 PmergeMe &PmergeMe::operator=(const PmergeMe &other) {
-    std::cout << "PmergeMe assignation operator" << std::endl;
     if (this == &other)
         return *this;
     return *this;
 }
 
-PmergeMe::~PmergeMe() { std::cout << "PmergeMe destructor" << std::endl; }
+PmergeMe::~PmergeMe() {}
